93a615a6477c20cfc263b606e147191d421f0d74
phasetr <phasetr@gmail.com>
2023-12-01 15:41:13 +0900

    commit

---
+++ 2023-12-01-aituber-book/openai_adapter.py
@@ -0,0 +1,37 @@
+import openai
+import dotenv
+import os
+
+# APIキーの設定
+dotenv.load_dotenv()
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+
+class OpenAIAdapter:
+    def __init__(self) -> None:
+        # system_promptはsystem_prompt.txtから読み込む
+        with open("system_prompt.txt", "r") as f:
+            self.system_prompt = f.read()
+        pass
+
+    def _create_message(self, role, message):
+        return {
+            "role": role,
+            "content": message
+        }
+
+    def create_chat(self, question):
+        system_message = self._create_message("system", self.system_prompt)
+        user_message = self._create_message("user", question)
+        messages = [system_message, user_message]
+        res = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+        )
+        return res["choices"][0]["message"]["content"]
+
+
+if __name__ == "__main__":
+    adapter = OpenAIAdapter()
+    response_text = adapter.create_chat("こんにちは")
+    print(response_text)
