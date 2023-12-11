a76aca5669f6737ea605ac3db8dfc8f49ed9a660
phasetr <phasetr@gmail.com>
2023-12-11 16:23:47 +0900

    commit

--- fundamentals/index.py
+++ fundamentals/index.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import poll
 import stt
 import tts
@@ -5,6 +6,10 @@ import tts
 tts = tts.TTS()
 stt = stt.STT()
 adapter = poll.OpenAIAdapter()
+
+# 開始時刻を取得
+start = datetime.now()
+start_str = datetime.now().strftime("%H:%M:%S.%f")
 
 # 読み込んだ文字列から音声ファイルを作る
 orig_audio_path = tts.create_audio_from_sample_text()
@@ -17,3 +22,12 @@ response_text = adapter.create_chat(text, orig_audio_path)
 # 回答を音声に変換する
 converted_audio_path = tts.create_audio(response_text)
 print(f"変換された音声ファイル: {converted_audio_path}")
+
+# 終了時刻を取得
+end = datetime.now()
+end_str = datetime.now().strftime("%H:%M:%S.%f")
+time_req = end - start
+# 各種時刻を表示
+print(f"start: {start_str}", flush=True)
+print(f"end:   {end_str}", flush=True)
+print(f"time required: {time_req}", flush=True)
